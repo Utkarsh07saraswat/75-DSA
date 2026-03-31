b773bb0b422469b64eac6ad92660d3469d2d59d0
Utkarsh <saraswatutkarsh04@gmail.com>
2026-04-01 00:15:06 +0530

    BestTimetoBuyandSellStock

--- TwoSum.java
+++ TwoSum.java
@@ -1,12 +1,19 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i=0 ; i<nums.length ; i++) {
-            for(int j=i+1 ; j<nums.length ; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i , j};
+public class TwoSum {
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+
+                    System.out.println("Indices: [" + i + ", " + j + "]");
+                    return; 
                 }
             }
         }
-        return new int[]{};
+
+        System.out.println("No solution found");
     }
 }
