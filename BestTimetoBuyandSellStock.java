5746bf2cb38f4cb857f3bec1e80087b344e59c1b
Utkarsh <saraswatutkarsh04@gmail.com>
2026-04-07 01:04:17 +0530

    Saving local files before pull

---
+++ BestTimetoBuyandSellStock.java
@@ -0,0 +1,17 @@
+public class BestTimetoBuyandSellStock {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int buy_price = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy_price) {
+                buy_price = prices[i];
+            } else {
+                int current_profit = prices[i] - buy_price; 
+                profit = Math.max(current_profit, profit);
+            }
+        }
+        System.out.println("Maximum Profit: " + profit); 
+    }
+}
